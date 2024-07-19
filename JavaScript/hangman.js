be79fd1bf3786f6f922c7c3d9d90fbb94aed7ef6
Stefan Redl <stefan.redl@isis-papyrus.com>
2024-07-19 16:53:47 +0200

    Input User

--- JavaScript/hangman.js
+++ JavaScript/hangman.js
@@ -4,19 +4,20 @@
 // Input vom User abfangen und mit dem Buchstaben vergleichen
 
 
+// User input
+let userInput = prompt('Enter letter: ');
+alert("Sie haben eingegeben: " + userInput);
 
+// List of words
 let words = ["apple", "orange", "pineapple"];
 
-// console.log(Math.floor(Math.random() * words.length));
-
+// Choose random word from the list and split it into letters
 let chosen_word = (words[(Math.floor(Math.random() * words.length))]);
-
 letter_from_chosen_word = chosen_word.split('');
-
 console.log(letter_from_chosen_word);
 
+// Replace underscores with the letters
 underscore_from_chosen_word = letter_from_chosen_word.map(letter=> " _ ").join('');
-
 console.log(underscore_from_chosen_word);
 
 
