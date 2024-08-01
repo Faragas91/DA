3d4a87c78b7df45f1031960b70a5fd261e9375eb
Stefan Redl <stefan.redl@isis-papyrus.com>
2024-08-01 12:06:36 +0200

    continue on hangman

--- JavaScript/hangman.js
+++ JavaScript/hangman.js
@@ -4,12 +4,18 @@
 // Input vom User abfangen und mit dem Buchstaben vergleichen
 
 
-// User input
-let userInput = prompt('Enter letter: ');
-alert("Sie haben eingegeben: " + userInput);
+// // User input
+// let userInput = prompt('Enter letter: ');
+// alert("Sie haben eingegeben: " + userInput);
 
-// List of words
+// Letters
+
+
+
+// Variable
 let words = ["apple", "orange", "pineapple"];
+let empty_letter = "";
+let alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 // Choose random word from the list and split it into letters
 let chosen_word = (words[(Math.floor(Math.random() * words.length))]);
@@ -19,6 +25,28 @@ console.log(letter_from_chosen_word);
 // Replace underscores with the letters
 underscore_from_chosen_word = letter_from_chosen_word.map(letter=> " _ ").join('');
 console.log(underscore_from_chosen_word);
+
+// Choose Random letters from the alphabet
+while (empty_letter.length < chosen_word.length) {
+    empty_letter = "";
+    empty_letter += alphabet[Math.floor(Math.random() * alphabet.length)];
+    empty_letter = "a"
+    for (let i = 0; i < chosen_word.length; i++) {
+        for (let j = 0; j < chosen_word.length; j++) {
+            console.log(empty_letter[j]);
+            console.log(letter_from_chosen_word[i])
+            if (empty_letter[j] == letter_from_chosen_word[i]){
+                guessed_letter = underscore_from_chosen_word.replace(underscore_from_chosen_word[i], empty_letter[j])
+                console.log(guessed_letter);
+            }
+            else {
+                break;
+            }
+        }
+    }
+}
+  
+
 
 
 
