8aede582be7f9a74023c6f23a8da76c1f4db5eae
Stefan Redl <bernhardlaus@gmx.at>
2024-09-04 20:03:45 +0200

    Kochwelt_20240904 Backup

--- Kochwelt_20240904/src/js/recipe_calculator_lee.js
+++ Kochwelt_20240904/src/js/recipe_calculator_lee.js
@@ -9,37 +9,28 @@ document.addEventListener('DOMContentLoaded', () => {
     // Ursprüngliche Werte der Zutaten speichern, um sie später basierend auf der Anzahl der Portionen zu multiplizieren
     const originalValues = Array.from(ingredientValues).map(item => parseFloat(item.textContent));
 
-    // Funktion zum Berechnen der Zutaten basierend auf der Portionsmenge
-    function calculateIngredients(multiplier) {
+    // Event-Listener für den Button hinzufügen
+    calculateButton.addEventListener('click', () => {
+        const multiplier = parseFloat(inputField.value); // Wert aus dem Eingabefeld holen und in eine Zahl umwandeln
+
+        // Eingabevalidierung: Überprüfen, ob der Wert eine gültige Zahl ist
+        // Die erste IF-Bedingung bräuchte es theoretisch nicht, da das Inputfeld nur Zahlen zulässt. Man weiss ja nie.
+        if (isNaN(multiplier)) {
+            alert('Bitte geben Sie eine gültige Zahl ein. Buchstaben sind nicht zulässig.'); 
+            inputField.value = 1;
+            return;
+        } 
+        // Überprüfen, ob der Wert innerhalb des erlaubten Bereichs (1 bis 20) liegt
+        else if (multiplier < 1 || multiplier > 20) {
+            alert('Bitte geben Sie eine Zahl zwischen 1 und 20 ein.');
+            inputField.value = 1;
+            return;
+        }
+
+        // Aktualisieren der Zutatenmengen basierend auf dem eingegebenen Multiplikator
         ingredientValues.forEach((item, index) => {
             const newValue = originalValues[index] * multiplier; // Neuer Wert wird berechnet
             item.textContent = newValue;
         });
-    }
-
-    // Event-Listener für den Button hinzufügen
-    calculateButton.addEventListener('click', () => {
-        let multiplier = parseFloat(inputField.value); // Wert aus dem Eingabefeld holen und in eine Zahl umwandeln
-
-        // Eingabevalidierung: Überprüfen, ob der Wert eine gültige Zahl ist
-        if (isNaN(multiplier)) {
-            alert('Bitte geben Sie eine gültige Zahl ein. Buchstaben sind nicht zulässig.');
-            inputField.value = 1;
-            multiplier = 1;
-            calculateIngredients(multiplier);
-            return;
-        }
-
-        // Überprüfen, ob der Wert innerhalb des erlaubten Bereichs (1 bis 20) liegt
-        if (multiplier < 1 || multiplier > 20) {
-            alert('Bitte geben Sie eine Zahl zwischen 1 und 20 ein.');
-            inputField.value = 1;
-            multiplier = 1;
-            calculateIngredients(multiplier);
-            return;
-        }
-
-        // Wenn der Wert gültig ist, Zutatenmengen basierend auf dem eingegebenen Multiplikator berechnen
-        calculateIngredients(multiplier);
     });
 });
