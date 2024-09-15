e05da036dd5539d401cbd68593c9875e64c72ca2
Stefan Redl <bernhardlaus@gmx.at>
2024-09-15 23:01:29 +0200

    Fotogram fixed the issue with the nextImage functions; fixed the closeImageButton issue; fixed the hover effect

--- Fotogram/src/js/display.js
+++ Fotogram/src/js/display.js
@@ -1,20 +1,16 @@
 let allImages = document.getElementsByClassName("picture__images");
 let overlay = document.getElementById("overlay");
 let currentIndex = 0;
-makeBigger = true; 
 
 for (let i = 0; i < allImages.length; i++) {
     let image = allImages[i];
     image.addEventListener('click', (event) => {
-      if (makeBigger) {
         // Hide all images and overlay
         hideImage();
         // Enlarge the clicked image
         biggerImage(image);
         // Update current index
         currentIndex = i;
-        makeBigger = false;
-      } 
     });
   }
 
@@ -26,6 +22,7 @@ function biggerImage(image) {
     image.style.zIndex = "100";
     overlay.style.display = "block";
 
+    // Create the button on the right side
     const nextButtonRight = document.createElement("img");
     nextButtonRight.src = "../../material_symbols/arrow_right.png";
     nextButtonRight.className = "next__button-right";
@@ -33,11 +30,18 @@ function biggerImage(image) {
 
     image.parentNode.appendChild(nextButtonRight)
 
+    // Listener acts when the next button is clicked
     nextButtonRight.addEventListener("click", () => {
+        image.classList.remove("image-enlarged"); 
+        image.classList.add("picture__image");
         image.style.display = "none";
+        image.parentNode.removeChild(nextButtonRight);
+        image.parentNode.removeChild(nextButtonLeft);
+        image.parentNode.removeChild(closeButton);
         nextImageRight();
     });
 
+    // Create the button on the left side
     const nextButtonLeft = document.createElement("img");
     nextButtonLeft.src = "../../material_symbols/arrow_left.png";
     nextButtonLeft.className = "next__button-left";
@@ -45,32 +49,35 @@ function biggerImage(image) {
 
     image.parentNode.appendChild(nextButtonLeft)
 
+    // Listener acts when the previous button is clicked
     nextButtonLeft.addEventListener("click", () => {
+        image.classList.remove("image-enlarged"); 
+        image.classList.add("picture__image");
         image.style.display = "none";
+        image.parentNode.removeChild(nextButtonRight);
+        image.parentNode.removeChild(nextButtonLeft);
+        image.parentNode.removeChild(closeButton);
         nextImageLeft();
 
     });
 
+    // Create the close button on the top right corner
     const closeButton = document.createElement("img");
-    closeButton.src = "../../material_symbols/arrow_left.png";
+    closeButton.src = "../../material_symbols/close.png";
     closeButton.className = "close__button";
     closeButton.style.zIndex = "100";
 
-    closeButton.addEventListener("click", () => {
-      image.style.display = "none";
-      console.log("Funktioniert");
+    image.parentNode.appendChild(closeButton);
 
-      
+    // Listener acts when the close button is clicked
+    closeButton.addEventListener("click", () => {
+      image.classList.remove("image-enlarged");
+      showAllImage();
+      image.parentNode.removeChild(nextButtonRight);
+      image.parentNode.removeChild(nextButtonLeft);
+      image.parentNode.removeChild(closeButton);
   });
 }
-// // Function to set the image to the normal settings
-// function normalizeImage(image) {
-//     image.style.display = "block";
-//     image.classList.add("image-normalized");
-//     image.style.zIndex = "0";
-//     overlay.style.display = "none";
-//     nextButtonLeft.style.display = "none";
-// }   
 
 // Hide all pictures and the overlay
 function hideImage() {
@@ -80,23 +87,26 @@ function hideImage() {
     overlay.style.display = "none";
 }
 
-// function showAllImage() {
-//     for (let i = 0; i < allImages.length; i++) {
-//       allImages[i].style.display = "flex";
-//     }
-//     overlay.style.display = "none";
-// }
+// Show all pictures again after the overlay is closed
+function showAllImage() {
+    for (let i = 0; i < allImages.length; i++) {
+      allImages[i].style.display = "flex";
+      allImages[i].classList.add("picture__images");
+    }
+    overlay.style.display = "none";
+}
 
-
+// Function to navigate to the next image when the "next" button is clicked
 function nextImageRight() {
     currentIndex++;
     if (currentIndex >= allImages.length) {
       currentIndex = 0;
     }
     const nextImage = allImages[currentIndex];
-    biggerImage(nextImage);
+    biggerImage(nextImage);   
 }
 
+// Function to navigate to the previous image when the "previous" button is clicked
 function nextImageLeft() {
     currentIndex--;
     if (currentIndex < 0) {
@@ -111,20 +121,3 @@ function toggleOverlay() {
     let overlayRef = document.getElementById("overlay");
     overlayRef.classList.toggle("d_on");
 }
-
-
-
-
-
-
-
-
-
-
-// else {
-//   // Show all images and overlay
-//   showAllImage();
-//   // Reset the image to its original size
-//   normalizeImage(image);
-//   makeBigger = true;
-// }
