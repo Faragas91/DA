08a6cb96a74bb6be2e0f0ff401482a4125615404
Stefan Redl <bernhardlaus@gmx.at>
2024-09-28 11:11:35 +0200

    Fotogram fixed feedback issues; splitted biggerImage in 6 functions; arrows now on the site from the picture

--- Fotogram/src/js/display.js
+++ Fotogram/src/js/display.js
@@ -22,54 +22,50 @@ for (let i = 0; i < allImages.length; i++) {
     });
 }
 
-// Function to bigger the image
-// Adds all neccessary buttons to the image (close, next, previous)
+// Main Function to enlarge the clicked image
 function biggerImage(image) {
     image.style.display = 'flex';
     image.classList.add("image-enlarged");
     image.style.zIndex = "100";
     overlay.style.display = "block";
 
-    // Create the button on the right side
+
+    // Function for the Buttons
+    createNextButtonRight(image);
+    createNextButtonLeft(image);
+    createCloseButton(image);
+}
+
+// Create the RightButton on the Image
+function createNextButtonRight(image) {
     const nextButtonRight = document.createElement("img");
     nextButtonRight.src = "../../material_symbols/arrow_right.png";
     nextButtonRight.className = "next__button-right";
-    nextButtonRight.style.zIndex = "100";
+    
+    image.parentNode.appendChild(nextButtonRight);
 
-    image.parentNode.appendChild(nextButtonRight)
-
-    // Listener acts when the next button is clicked
     nextButtonRight.addEventListener("click", () => {
-        image.classList.remove("image-enlarged"); 
-        image.classList.add("picture__image");
-        image.style.display = "none";
-        image.parentNode.removeChild(nextButtonRight);
-        image.parentNode.removeChild(nextButtonLeft);
-        image.parentNode.removeChild(closeButton);
+        removeEnlargedElements(image, nextButtonRight);
         nextImageRight();
     });
+}
 
-    // Create the button on the left side
+// Create the LeftButton on the Image
+function createNextButtonLeft(image) {
     const nextButtonLeft = document.createElement("img");
     nextButtonLeft.src = "../../material_symbols/arrow_left.png";
     nextButtonLeft.className = "next__button-left";
-    nextButtonLeft.style.zIndex = "100";
 
-    image.parentNode.appendChild(nextButtonLeft)
+    image.parentNode.appendChild(nextButtonLeft);
 
-    // Listener acts when the previous button is clicked
     nextButtonLeft.addEventListener("click", () => {
-        image.classList.remove("image-enlarged"); 
-        image.classList.add("picture__image");
-        image.style.display = "none";
-        image.parentNode.removeChild(nextButtonRight);
-        image.parentNode.removeChild(nextButtonLeft);
-        image.parentNode.removeChild(closeButton);
+        removeEnlargedElements(image, nextButtonLeft);
         nextImageLeft();
-
     });
+}
 
-    // Create the close button on the top right corner
+// Create the CloseButton on the Image
+function createCloseButton(image) {
     const closeButton = document.createElement("img");
     closeButton.src = "../../material_symbols/close.png";
     closeButton.className = "close__button";
@@ -77,17 +73,23 @@ function biggerImage(image) {
     
     image.parentNode.appendChild(closeButton);
 
-    // The listener acts when the “Close” button is clicked
-    // It removes all buttons, the image enlarger and switches back to the main menu
     closeButton.addEventListener("click", () => {
       image.classList.remove("image-enlarged");
       showAllImage();
-      image.parentNode.removeChild(nextButtonRight);
-      image.parentNode.removeChild(nextButtonLeft);
-      image.parentNode.removeChild(closeButton);
-      // Sets the variable back to true to use the image magnification for another image again
-      makePictureBigger = true
-  });
+      removeEnlargedElements(image, closeButton);
+      makePictureBigger = true;
+    });
+}
+
+// Function to remove all Buttons on the Image
+function removeEnlargedElements(image) {
+    image.classList.remove("image-enlarged"); 
+    image.classList.add("picture__image");
+    image.style.display = "none";
+
+    // Collects all classes from the buttons and then removes them
+    const buttons = image.parentNode.querySelectorAll('.next__button-right, .next__button-left, .close__button');
+    buttons.forEach(button => button.parentNode.removeChild(button));
 }
 
 // Hide all pictures and the overlay
