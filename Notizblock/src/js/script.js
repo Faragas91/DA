bc84d78062e573f217b73fce3c3540e69782365f
Stefan <bernhardlaus@gmx.at>
2024-10-12 19:49:03 +0200

    Notizblock fixed input issue

--- Notizblock/src/js/script.js
+++ Notizblock/src/js/script.js
@@ -34,16 +34,63 @@ function renderTrashNotes() {
 function addNote(){
     let noteInputTitleRef = document.getElementById('note_title_input');
     let noteInputTitle = noteInputTitleRef.value;
-    notesTitles.push(noteInputTitle);
-    noteInputTitleRef.value = "";
 
     let noteInputTaskRef = document.getElementById('note_task_input');
     let noteInputTask = noteInputTaskRef.value;
+
+    if (noteInputTitle === "" || noteInputTask === "") {
+        showError(noteInputTitle, noteInputTask);
+        return;
+    }
+
+    notesTitles.push(noteInputTitle);
+    noteInputTitleRef.value = "";
+
     notes.push(noteInputTask);
     noteInputTaskRef.value = "";
 
+    showSuccessMessage();  // Nach dem Hinzufügen einer Notiz eine Meldung anzeigen
+
+    noteInputTitleRef.classList.remove('error');
+    document.getElementById('title-error').innerText = "";
+
+    noteInputTaskRef.classList.remove('error');
+    document.getElementById('task-error').innerText = "";
+   
     renderNotes();  // Nach dem Hinzufügen von Notizen rendern und speichern
 }
+
+function showError(isTitleEmpty, isTaskEmpty) {
+    let noteInputTitleRef = document.getElementById('note_title_input');
+    let noteInputTaskRef = document.getElementById('note_task_input');
+
+    if (isTitleEmpty) {
+        noteInputTitleRef.classList.add('error');
+        document.getElementById('title-error').innerText = "Bitte einen Titel eingeben";
+        }
+        else {
+            noteInputTitleRef.classList.remove('error');
+            document.getElementById('title-error').innerText = "";
+        }
+    if (isTaskEmpty) {
+        noteInputTaskRef.classList.add('error');
+        document.getElementById('task-error').innerText = "Bitte eine Aufgabe eingeben";
+        }
+        else {
+            noteInputTaskRef.classList.remove('error');
+            document.getElementById('task-error').innerText = "";
+        }
+}
+
+function showSuccessMessage() {
+    let messageContainer = document.getElementById('success-message');
+    messageContainer.innerText = "Notiz erfolgreich hinzugefügt";
+    messageContainer.style.display = "block";
+
+    setTimeout(() => {
+        messageContainer.style.display = "none";
+    }, 2000);
+};
 
 function pushToTrash(indexNote) {
     let trashNote = notes.splice(indexNote, 1);
