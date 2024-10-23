212bd835cb6f7a386dbc95d7dd234b7d66e643a9
Stefan <bernhardlaus@gmx.at>
2024-10-23 13:55:42 +0200

    Book Store details like a table; configure the like button

--- Book Store/src/js/script.js
+++ Book Store/src/js/script.js
@@ -3,3 +3,40 @@ const bookContainers = books.map((book, index) => templateContainer(book, index)
 document.querySelector(".book__main-container").innerHTML = bookContainers.join("");
 
 
+images = [""]
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bilder Tauschen</title>
+    <style>
+        img {
+            width: 200px; /* Beispielgröße für die Bilder */
+            height: auto;
+        }
+    </style>
+</head>
+<body>
+    <img id="image1" src="bild1.jpg" alt="Bild 1">
+    <img id="image2" src="bild2.jpg" alt="Bild 2">
+    <button onclick="swapImages()">Bilder Tauschen</button>
+
+    <script>
+        function swapImages() {
+            const img1 = document.getElementById('image1');
+            const img2 = document.getElementById('image2');
+
+            // Temporäre Speicherung des src-Werts von Bild 1
+            const tempSrc = img1.src;
+
+            // Tauschen der src-Werte
+            img1.src = img2.src;
+            img2.src = tempSrc;
+        }
+    </script>
+</body>
+</html>
+
+
--- Book Store/src/js/template.js
+++ Book Store/src/js/template.js
@@ -6,16 +6,29 @@ function templateContainer(book, index) {
         <img class="book__logo" src="./assets/img/book-logo.png" alt="Logo">
         <div class="divider"></div>
         <div class="book__price-and-like">
-            <p id="book-price">Preis: €${book.price.toFixed(2)}</p>
-            <p id="book-likes">${book.likes} Likes</p>
-            <button class="book_like-button" id="like-button-${index}">
-                ${book.liked ? 'Unlike' : 'Like'}
-            </button>
+            <p id="book-price">€${book.price.toFixed(2)}</p>
+            <div class="book__likes">
+                <p id="book-likes">${book.likes}</p>
+                <button id="like-button-${index}">
+                    ${book.liked ? 'Unlike' : 'Like'}
+                </button>
+            </div>
         </div>
         <div class="book__details">
-            <p id="book-author">Autor: ${book.author}</p>
-            <p id="book-genre">Genre: ${book.genre}</p>
-            <p id="book-published">Veröffentlicht: ${book.publishedYear}</p>
+            <table>
+                <tr>
+                    <td class="book__tr-right">Author:</td>
+                    <td>${book.author}</td>
+                </tr>
+                <tr>
+                    <td class="book__tr-right">Genre:</td>
+                    <td>${book.genre}</td>
+                </tr>
+                <tr>
+                    <td class="book__tr-right">Veröffentlicht:</td>
+                    <td>${book.publishedYear}</td>
+                </tr>
+            </table>
         </div>
         <div class="divider"></div>
         <h3>Kommentare</h3>
