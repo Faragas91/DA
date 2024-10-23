c946bc0f9fab17abcd42e72cd72dfb683d38065e
Stefan Redl <bernhardlaus@gmx.at>
2024-10-24 00:17:34 +0200

    Book Store designed the button; switch from like to unlike an reverse

--- Book Store/src/js/template.js
+++ Book Store/src/js/template.js
@@ -1,19 +1,5 @@
-function templateContainer(book, index) {
+function bookDetails(book) {
     return `
-    <div class="book__container" id="book-container-${index}">
-        <h2 class="book__title" id="book-title">${book.name}</h2>
-        <div class="divider"></div>
-        <img class="book__logo" src="./assets/img/book-logo.png" alt="Logo">
-        <div class="divider"></div>
-        <div class="book__price-and-like">
-            <p id="book-price">€${book.price.toFixed(2)}</p>
-            <div class="book__likes">
-                <p id="book-likes">${book.likes}</p>
-                <button id="like-button-${index}">
-                    ${book.liked ? 'Unlike' : 'Like'}
-                </button>
-            </div>
-        </div>
         <div class="book__details">
             <table>
                 <tr>
@@ -30,12 +16,47 @@ function templateContainer(book, index) {
                 </tr>
             </table>
         </div>
-        <div class="divider"></div>
+        `;
+}
+
+function bookPriceAndLike(book, index) {
+    return `
+        <div class="book__price-and-like">
+            <p id="book-price">€${book.price.toFixed(2)}</p>
+            <div class="book__likes">
+                <p id="book-likes">${book.likes}</p>
+                <div class="${book.liked ? 'book__liked' : 'book__like'}" 
+                     onclick="swapImages(${index})" 
+                     id="like-${index}">
+                </div>
+            </div>
+        </div>
+        `;
+}
+
+function bookComments(book, index) {
+    return `
         <h3>Kommentare</h3>
         <ul>
             ${book.comments.map(comment => `<li>${comment}</li>`).join('')}
         </ul>
         <input type="text" id="commit-${index}" name="commit" placeholder="Neuer Kommentar">
+        `;
+}
+
+
+function templateContainer(book, index) {
+    return `
+    <div class="book__container" id="book-container-${index}">
+        <h2 class="book__title" id="book-title">${book.name}</h2>
+        <div class="divider"></div>
+        <img class="book__logo" src="./assets/img/book-logo.png" alt="Logo">
+        <div class="divider"></div>
+        ${bookPriceAndLike(book, index)}
+        <div class="divider"></div>
+        ${bookDetails(book)}
+        <div class="divider"></div>
+        ${bookComments(book, index)}
     </div>
     `;
 };
