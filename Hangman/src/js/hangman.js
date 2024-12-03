fc0a182e61a86bb1617742f684deef87f2088726
Stefan Redl <stefan.redl@isis-papyrus.com>
2024-12-03 07:01:27 +0100

    Hangman; design Letter; make Picture for Hangman;

--- Hangman/src/js/hangman.js
+++ Hangman/src/js/hangman.js
@@ -1,11 +1,33 @@
-let letterButtons = '';
-let letter;
-for (let i = 65; 90 >= i; i++) {// A-65, Z-90
-  letter = String.fromCharCode(i);
-  letterButtons += '<button onclick="setLetter(\'' + letter + '\');">' + letter + '</button>';
+const pictures = [
+  'hangman_0.png',
+  'hangman_1.png',
+  'hangman_2.png',
+  'hangman_3.png',
+  'hangman_4.png',
+  'hangman_5.png',
+  'hangman_6.png',
+  'hangman_7.png',
+  'hangman_8.png',
+  'hangman_9.png',
+  'hangman_10.png',
+  'hangman_11.png',
+];
+function createLetterButtons() {
+    let letterButtons = '';
+    let letter;
+    for (let i = 65; 90 >= i; i++) {// A-65, Z-90
+      letter = String.fromCharCode(i);
+      letterButtons += '<button class="hangman__letter-button" onclick="setLetter(\'' + letter + '\');">' + letter + '</button>';
+    }
+    document.getElementById('box').innerHTML = letterButtons;
 }
-document.getElementById('box').innerHTML = letterButtons;
 
-let setLetter = function(x) {
-  document.getElementById('name').innerHTML += x;
+let setLetter = function(letter) {
+  document.getElementById('name').innerHTML += letter;
 };
+
+
+function generatePicture() {
+  const currentPicture = 0;
+  
+}
