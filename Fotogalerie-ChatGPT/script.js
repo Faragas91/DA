297df9d269ba241508ae580862f208f889148329
Stefan Redl <stefan.redl@isis-papyrus.com>
2024-12-04 07:00:11 +0100

    Foto; fixed issue with pictures

--- Fotogalerie-ChatGPT/script.js
+++ Fotogalerie-ChatGPT/script.js
@@ -1,23 +1,23 @@
 const pictures = [
-    'cat.jpg',
-    // './assets/img/chameleon.jpg',
-    // './assets/img/cows.jpg',
-    // './assets/img/dogs.jpg',
-    // './assets/img/ducks.jpg',
-    // './assets/img/elephant.jpg',
-    // './assets/img/fox.jpg',
-    // './assets/img/giraffes.jpg',
-    // './assets/img/pigs.jpg',
-    // './assets/img/rhino.jpg',
-    // './assets/img/tigers.jpg',
-    // './assets/img/wolves.jpg',
+    '/assets/img/cat.jpg',
+    '/assets/img/chameleon.jpg',
+    '/assets/img/cows.jpg',
+    '/assets/img/dogs.jpg',
+    '/assets/img/ducks.jpg',
+    '/assets/img/elephant.jpg',
+    '/assets/img/fox.jpg',
+    '/assets/img/giraffes.jpg',
+    '/assets/img/pigs.jpg',
+    '/assets/img/rhino.jpg',
+    '/assets/img/tigers.jpg',
+    '/assets/img/wolves.jpg',
 ];
 
 function render() {
-    const container = document.getElementById('container').innerHTML
+    let container = document.getElementById('container')
     let images = "";
     for (let i = 0; i < pictures.length; i++) {
-        images += `<img src="${pictures[i]}">`;
+        images += `<img class="pictures" src="${pictures[i]}">`;
     }
     container.innerHTML = images;
 }
